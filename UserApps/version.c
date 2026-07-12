200d3d2e380e9b36d1e5fca07a3b5ed9b94e2ead
Grovvik <grovvik.0@gmail.com>
2026-07-12 14:30:51 +0300

    feat(UserApps): version

--- UserApps/version.c
+++ UserApps/version.c
@@ -1,7 +1,10 @@
 #include "userlib.h"
 
 void main(const char* args, const char* cwd, i32 argc) {
+    char os_version[64] = "unknown";
+    syscall2(SYS_SYSTEM, SYS_SYS_VERSION, (u64)(usize)os_version);
+
     setcolor(FB_YELLOW, FB_BLACK);
-    print("MicroNT v0.3 (Phase 2 - Ring3 + AHCI + FAT)\n");
+    printf("MicroNT %s\n", os_version);
     setcolor(FB_WHITE, FB_BLACK);
 }
